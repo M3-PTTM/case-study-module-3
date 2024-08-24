799d6071d116d0e255e76078c1479802a3303081
Admin <lmht721@gmail.com>
2024-08-24 19:33:26 +0700

    update login-register

--- src/phuc/js/login-register.js
+++ src/phuc/js/login-register.js
@@ -3,9 +3,9 @@ let show_btn = document.querySelector(".show");
 show_btn.addEventListener("click", function () {
     if (pass_field.type === "password") {
         pass_field.type = "text";
-        show_btn.innerHTML = "<em class='bx bx-hide' ></em>";
+        show_btn.innerHTML = "<em class=\"fa-solid fa-eye-slash\"></em>";
     } else {
         pass_field.type = "password";
-        show_btn.innerHTML = "<em class='bx bx-show'></em>";
+        show_btn.innerHTML = "<em class=\"fa-solid fa-eye\"></em>";
     }
 });
