766ce13ec9ca3e6d2976294986f6d4e9e4daba96
Admin <lmht721@gmail.com>
2024-09-06 15:33:05 +0700

    asdfasfafdasd

--- src/main/java/com/example/m3_g3_assignment/servlet/RegisterController.java
+++ src/main/java/com/example/m3_g3_assignment/servlet/RegisterController.java
@@ -1,9 +1,7 @@
 package com.example.m3_g3_assignment.servlet;
 
-
 import com.example.m3_g3_assignment.dao.impl.CustomerDAO;
 import com.example.m3_g3_assignment.model.Customer;
-import org.mindrot.jbcrypt.BCrypt;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
